e1392760914229a97d35d7655c760cb1cdf1110a
carlwilliamrix <carlrix1@gmail.com>
2025-10-22 20:15:04 +0200

    add initial test application

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
-</template>
+<script lang="ts" setup>
+import { ref } from 'vue';
 
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
+// State variables, to bind input to state variables see in HTML
+// Built in directive is v-model
+// Pass in a ref, with input this will update the ref value on input change
+// Adding a change handler and updating the value
+const message = ref("Welcome to Spaces.");
+const eventName = ref("");
 
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
+// We want to listen when this form is submitted
+// For that we use the @ sign to add a DOM event
+// When event is triggered we call this function
+// We must use .value to get the value from a ref since it is essentially a object wrapper around a primitive value
+function formSubmited() {
+  alert(`Creating event: ${eventName.value}`);
+}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<template>
+  <main>
+    <h1>{{ message }} </h1>
+    <form @submit.prevent="formSubmited">
+      <label>
+        New Event       
+        <input v-model="eventName" name="event-name" placeholder="Event Name" >
+      </label>
+      <div class="button-container">
+        <button>Create Event: {{ eventName }}</button>
+      </div>
+    </form>
+  </main>  
+</template>
+
+<style scoped>
+main {
+  max-width: 800px;
+  margin: 1rem auto;
 }
+
 </style>
+
+<!--
+Scoped CSS to this file.
+-->
+<style scoped>
+main {
+  max-width: 800px;
+  margin: 1rem auto;
+}
+
+.button-container {
+  display: flex;
+  justify-content: end;
+}
+</style> 
